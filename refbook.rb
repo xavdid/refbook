e7d72190228f8fcf26c6f6a9ad3b0edbc826c469
David Brownman <beamneocube@gmail.com>
2014-07-12 05:52:42 -0700

    fixed northwest region key

--- refbook.rb
+++ refbook.rb
@@ -17,7 +17,7 @@ configure do
   enable :sessions
   
   set :session_secret, 'this_is_secret'
-  set :region_hash, {"QuidditchUK" => "QUK","US Pacific Northwest" => "USPN","US West" => "USWE", "US Midwest" => "USMW", "US Southwest" => "USSW", "US South" => "USSO", "US Northeast" => "USNE", "US Mid-Atlantic" => "USMA", "Canada" => "CANA", "Australia" => "AUST", "Italy" => "ITAL", "All Regions" => "ALL","None" => "NONE"}
+  set :region_hash, {"QuidditchUK" => "QUK","US Northwest" => "USNW","US West" => "USWE", "US Midwest" => "USMW", "US Southwest" => "USSW", "US South" => "USSO", "US Northeast" => "USNE", "US Mid-Atlantic" => "USMA", "Canada" => "CANA", "Australia" => "AUST", "Italy" => "ITAL", "All Regions" => "ALL","None" => "NONE"}
   set :region_names, settings.region_hash.keys[0..-3].sort
   set :region_codes, settings.region_hash.values[0..-3].sort
   # TIME BETWEEN ATTEMPTS
