8a95cad31a7ff4bf94d6054c8594796a94274eb6
David Brownman <beamneocube@gmail.com>
2014-09-24 22:35:12 -0400

    adding tests for days

--- spec/test.rb
+++ spec/test.rb
@@ -9,8 +9,120 @@ class RefbookTest < MiniTest::Test
   def app
     Sinatra::Application
   end
+  
+  def setup
+    @user_cookie = {user: {}}
+  end
 
-  # need to call functions test_XXXX
+  def set_en
+    @user_cookie[:user].merge!("lang" => "EN")
+  end
+
+  def set_fr
+    @user_cookie[:user].merge!("lang" => "FR")
+  end
+
+  def set_it
+    @user_cookie[:user].merge!("lang" => "IT")
+  end
+
+  def paid
+    @user_cookie[:user].merge!("paid" => true)
+  end
+
+  # Test that all pages load and have text in each language
+
+  def test_en_404
+    get '/qwere'
+    assert last_response.body.include?('exist')
+  end
+
+  def test_en_500
+    get '/break'
+    assert last_response.body.include?('wrong')
+  end
+  
+  def test_en_about
+    get '/about'
+    assert last_response.body.include?('utilizing')
+  end
+  
+  def test_en_contact
+    get '/contact'
+    assert last_response.body.include?('shared')
+  end
+  
+  def test_en_create
+    get '/create'
+    assert last_response.body.include?('correct')
+  end
+  
+  def test_en_faq
+    get '/faq'
+    assert last_response.body.include?('offered')
+  end
+  
+  def test_en_index
+    get '/'
+    assert last_response.body.include?('referees around the world')
+  end
+  
+  def test_en_info
+    get '/info'
+    assert last_response.body.include?('formation')
+  end
+  
+  def test_en_login
+    get '/login'
+    assert last_response.body.include?('Forgot')
+  end
+  
+  def test_en_pay
+    get '/pay'
+    assert last_response.body.include?('to make a purchase')
+  end
+  
+  def test_en_public_profile
+    get '/profile/SOMETHING'
+    assert last_response.body.include?('review for')
+  end
+  
+  def test_en_review
+    get '/review'
+    assert last_response.body.include?('anonymous')
+  end
+  
+  def test_en_personal_review
+    get '/review/SOMETHING'
+    assert last_response.body.include?('David')
+  end
+  
+  def test_en_search
+    get '/search/ALL'
+    assert last_response.body.include?('currently')
+  end
+  
+  def test_en_search_usmw
+    get '/search/USMW'
+    assert last_response.body.include?('West')
+  end
+  
+  def test_en_testing
+    get '/testing'
+    assert last_response.body.include?('funds for thd IRDP')
+    assert last_response.body.include?('use to evaluate')
+  end
+
+  def test_unpaid
+    # telling me about paid content
+    # should have affiliate have affiliate register bugger
+  end
+
+  def test_paid
+    # shouldn't have help
+    # should have hidden guides
+  end
+
   def test_up
     get '/'
     assert last_response.ok?
@@ -18,7 +130,7 @@ class RefbookTest < MiniTest::Test
 
   def test_it_says_hello_world
     get '/', {}, "rack.session" => {user: {"lang" => "FR"}}
-    assert last_response.body.include?('Accueil'), 'body had wrong text'
+    assert last_response.body.include?('Accueil')
   end
 
   # def test_it_says_hello_to_a_person
