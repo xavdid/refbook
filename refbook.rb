38529d40262b084a7aa252bd0aadf062b561c26b
David Brownman <beamneocube@gmail.com>
2014-10-29 01:52:11 -0700

    Added new leagues to faq

--- refbook.rb
+++ refbook.rb
@@ -17,8 +17,9 @@ configure do
   enable :sessions
   
   set :session_secret, 'this_is_secret'
-  set :region_hash, {"QuidditchUK" => "QUK","US Northwest" => "USNW","US West" => "USWE", "US Midwest" => "USMW", "US Southwest" => "USSW", "US South" => "USSO", "US Northeast" => "USNE", "US Mid-Atlantic" => "USMA", "Canada" => "CANA", "Australia" => "AUST", "Italy" => "ITAL", "Norway" => "NORW", "Belgium" => "BELG", "Muggle Quidditch Netherlands" => "MQN", "Other" => "OTHR", "All Regions" => "ALL","None" => "NONE"}
-  set :affiliate, ["QUK", "AUST", "CANA", "ITAL", "NORW"]
+  # this really needs to be redone
+  set :region_hash, {"QuidditchUK" => "QUK","US Northwest" => "USNW","US West" => "USWE", "US Midwest" => "USMW", "US Southwest" => "USSW", "US South" => "USSO", "US Northeast" => "USNE", "US Mid-Atlantic" => "USMA", "Canada" => "CANA", "Australia" => "AUST", "Italy" => "ITAL", "Norway" => "NORW", "Belgium" => "BQF", "Netherlands" => "MQN", "Other" => "OTHR", "All Regions" => "ALL","None" => "NONE"}
+  set :affiliate, ["QUK", "AUST", "CANA", "ITAL", "NORW", "BQF", "MQN"]
   # this is actually a little dumb because it's just lucky that both lists are the first letters and sort the same way. 
   set :region_names, settings.region_hash.keys[0..-3].sort
   set :region_codes, settings.region_hash.values[0..-3].sort
